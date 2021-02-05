b73abfcc10dff345d5dd3831f47ade21f7605308
DESKTOP-MN018R4\Thyme <koontime@gmail.com>
2021-02-05 19:52:04 +0700

    SendHomeworkWeek2

---
+++ GI455_HW/Homework/Assets/HW_Week1/Scripts/TextData.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextData : MonoBehaviour
+{
+    public List<string> data = new List<string>();
+    string display = "";
+    public Text showText;
+    public Text searchText;
+    public Text showReseach;
+    
+
+    void Start()
+    {
+        data = new List<string>();
+        
+        data.Add("Thyme");
+        data.Add("Por");
+        data.Add("Namtan");
+        data.Add("Yok");
+        data.Add("Bright");
+ 
+        ShowText();
+        
+    }
+
+    public void ShowText()
+    {
+        foreach (string dataList in data)
+        {
+            display = display.ToString() + dataList.ToString() + "\n";
+        }
+
+        showText.text = display;
+        
+    }
+
+    public void checkData()
+    {
+        if (data.Contains(searchText.text))
+        {
+            showReseach.text = string.Format("Found <color=green>{0}</color> in data", searchText.text);
+        }
+        
+        else
+        {
+            showReseach.text = string.Format("Not Found <color=red>{0}</color> in data", searchText.text);
+        }
+
+    }
+
+}
---
+++ GI455_HW/Homework/Assets/ProgramChat/Scripts/Websocket_Chat.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+using UnityEngine.UI;
+
+
+namespace ProgramChat_Hw
+{
+    public class Websocket_Chat: MonoBehaviour
+    {
+        
+        public Text chatText;
+        public Text userText;
+        public Text coUserText;
+        public InputField ip;
+        public InputField port;
+        private bool checkChatUpdate = false;
+        public bool checkIsChatUser = false;
+        
+        public GameObject bgLogin;
+
+        private WebSocket websocket;
+
+
+        void Start()
+        {
+          
+        }
+
+        void Update()
+        {
+
+
+        }
+        private void OnDestroy()
+        {
+            if (websocket != null)
+            {
+                websocket.Close();
+            }
+        }
+
+        public void OnMessage(object sender, MessageEventArgs messageEventArgs)
+        {
+            Debug.Log("Chat : " + messageEventArgs.Data);
+            //coChat = messageEventArgs.Data;
+
+            if (checkIsChatUser == false)
+            {
+                coUserText.text += messageEventArgs.Data + "\n";
+                checkChatUpdate = true;
+            }
+            else
+            {
+                checkIsChatUser = false;
+            }
+        }
+
+
+        public void SendButton()
+        {
+            checkIsChatUser = true;
+            chatText.text += userText.text + "\n";
+            ChatToSever();
+        }
+
+        public void ChatToSever()
+        {
+            if (websocket.ReadyState == WebSocketState.Open)
+            {
+                websocket.Send(userText.text);
+            }
+
+        }
+
+        public void LoginButton()
+        {
+            websocket = new WebSocket($"ws://{ip.text}:{port.text}/");
+            //websocket = new WebSocket("ws://127.0.0.1:5500/");
+            websocket.OnMessage += OnMessage;
+            websocket.Connect();
+            bgLogin.SetActive(false);
+        }
+
+    }
+}
+
